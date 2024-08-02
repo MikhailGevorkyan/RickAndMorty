f7b6048fc3568c534f0b5542c3b361ec020f1e67
MikhailGevorkyan <gevorkyan.fortech@gmail.com>
2024-08-02 17:48:44 +0300

    feat: store update

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -1,5 +1,10 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  createApi,
+  fetchBaseQuery,
+  defaultSerializeQueryArgs,
+} from "@reduxjs/toolkit/query/react";
 import { REACT_APP_BASE_URL } from "../../config";
+import { updateCharacters } from "../charactersSlice";
 
 const baseUrl = REACT_APP_BASE_URL;
 
@@ -10,47 +15,54 @@ export const apiSlice = createApi({
     getCharacters: builder.query({
       query: ({ name, page = 1 }: { name: string; page: number }) =>
         `/character/?page=${page}&name=${name}`,
-      // serializeQueryArgs: ({ endpointName }) => {
-      //   return endpointName;
-      // },
-      // // Always merge incoming data to the cache entry
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      // Always merge incoming data to the cache entry
       // merge: (currentCache, newItems) => {
+      //   console.log(currentCache);
       //   currentCache.push(...newItems);
       // },
-      // // Refetch when the page arg changes
-      // forceRefetch({ currentArg, previousArg }) {
-      //   return currentArg !== previousArg;
-      // },
+      // Refetch when the page arg changes
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
+      async onQueryStarted(_arg, { dispatch, queryFulfilled }) {
+        const result = await queryFulfilled;
+        const data = result.data;
+
+        dispatch(updateCharacters(data.news));
+      },
     }),
     getLocations: builder.query({
       query: ({ name, page = 1 }: { name: string; page: number }) =>
         `/location/?page=${page}&name=${name}`,
-      // serializeQueryArgs: ({ endpointName }) => {
-      //   return endpointName;
-      // },
-      // // Always merge incoming data to the cache entry
-      // merge: (currentCache, newItems) => {
-      //   currentCache.push(...newItems);
-      // },
-      // // Refetch when the page arg changes
-      // forceRefetch({ currentArg, previousArg }) {
-      //   return currentArg !== previousArg;
-      // },
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      // Always merge incoming data to the cache entry
+      merge: (currentCache, newItems) => {
+        currentCache.push(...newItems);
+      },
+      // Refetch when the page arg changes
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
     getEpisodes: builder.query({
       query: ({ name, page = 1 }: { name: string; page: number }) =>
         `/episodes/?page=${page}&name=${name}`,
-      // serializeQueryArgs: ({ endpointName }) => {
-      //   return endpointName;
-      // },
-      // // Always merge incoming data to the cache entry
-      // merge: (currentCache, newItems) => {
-      //   currentCache.push(...newItems);
-      // },
-      // // Refetch when the page arg changes
-      // forceRefetch({ currentArg, previousArg }) {
-      //   return currentArg !== previousArg;
-      // },
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      // Always merge incoming data to the cache entry
+      merge: (currentCache, newItems) => {
+        currentCache.push(...newItems);
+      },
+      // Refetch when the page arg changes
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
     }),
   }),
 });
